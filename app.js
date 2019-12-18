0993bad886c1f49a4937c8a8eb25dbc9a1d2136c
v_yougwang <v_yougwang@tencent.com>
2019-12-18 17:01:14 +0800

    koa-cookie

--- app.js
+++ app.js
@@ -10,7 +10,19 @@ const staticPath = './static'
 app.use(static(path.join(__dirname, staticPath)))
 
 app.use(async (ctx) => {
-  ctx.body = 'hello koa-static'
+  if (ctx.url === '/index') {
+    ctx.cookies.set('koa-cookie', 'hello koa-cookie', {
+      domain: 'localhost',  // 写cookie所在的域名
+      path: '/index',       // 写cookie所在的路径
+      maxAge: 10 * 60 * 1000, // cookie有效时长
+      expires: new Date('2017-02-15'),  // cookie失效时间
+      httpOnly: false,  // 是否只用于http请求中获取
+      overwrite: false  // 是否允许重写
+    })
+    ctx.body = 'cookie is ok'
+  } else {
+    ctx.body = 'cookie not ok'
+  }
 })
 
 app.listen(3000)
