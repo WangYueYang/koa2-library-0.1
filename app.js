50f3f8da1323036239cb5f70e53181e1e8306c88
v_yougwang <v_yougwang@tencent.com>
2019-12-18 20:22:12 +0800

    koa 原生 jsonp

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
 const Koa = require('koa')
-const path = require('path')
 const app = new Koa()
-
-const { uploadFile } = require('./util/upload')
 
 
 app.use(async (ctx) => {
-  if (ctx.url === '/' && ctx.method === 'GET') {
-    // 当GET请求时候返回表单页面
-    let html = `
-      <h1>koa2 upload demo</h1>
-      <form method="POST" action="/upload.json" enctype="multipart/form-data">
-        <p>file upload</p>
-        <span>picName:</span><input name="picName" type="text" /><br/>
-        <input name="file" type="file" /><br/><br/>
-        <button type="submit">submit</button>
-      </form>
-    `
-    ctx.body = html
+  if (ctx.method === 'GET' && ctx.url.split('?')[0] === '/getData.jsonp') {
+    console.log(ctx)
+    let callbackName = ctx.query.callback || 'callback'
+    let returnData = {
+      success: true,
+      data: {
+        text: 'this is a jsonp api',
+        time: new Date().getTime(),
+      }
+    }
 
-  } else if (ctx.url === '/upload.json' && ctx.method === 'POST') {
-    // 上传文件请求处理
-    let result = { success: false }
-    let serverFilePath = path.join(__dirname, 'upload-files')
+    let jsonpStr = `;${callbackName}(${JSON.stringify(returnData)})`
 
-    // 上传文件事件
-    result = await uploadFile(ctx, {
-      fileType: 'album', // common or album
-      path: serverFilePath
-    })
+    ctx.type = 'text/javascript'
 
-    ctx.body = result
+    ctx.body = jsonpStr
+  } else {
+    ctx.body = 'hellp jsonp'
   }
 })
 
