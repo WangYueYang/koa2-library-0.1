ecbda656d60d0fe21ce81c466824be06677dc224
v_yougwang <v_yougwang@tencent.com>
2019-12-18 16:24:50 +0800

    koa-static 中间件

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
 const Koa = require('koa')
 const path = require('path')
-
-const content = require('./util/content')
-const mimes = require('./util/mimes')
+const static = require('koa-static')
 
 const app = new Koa()
 
 const staticPath = './static'
 
-function parseMime(url) {
-  let extName = path.extname(url) // 返回path拓展名
-  extName = extName ? extName.slice(1) : 'unKnown'
-  return mimes[extName]
-}
+
+app.use(static(path.join(__dirname, staticPath)))
 
 app.use(async (ctx) => {
-  let fullStaticPath = path.join(__dirname, staticPath)
-
-  let _content = await content(ctx, fullStaticPath)
-
-  let _mime = parseMime(ctx.url)
-
-  if (_mime) {
-    ctx.type = _mime
-  }
-
-  if (_mime && _mime.indexOf('img/') >= 0) {
-    ctx.res.writeHead(200)
-    ctx.res.write(_content, 'binary')
-    ctx.res.end()
-  } else {
-    ctx.body = _content
-  }
+  ctx.body = 'hello koa-static'
 })
 
 app.listen(3000)
