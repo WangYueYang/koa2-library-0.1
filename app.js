5272ec02dc2dc2bfc14141c4e0f78612e100bc7e
v_yougwang <v_yougwang@tencent.com>
2019-12-18 15:39:19 +0800

    koa-bodyparser中间件

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const Koa = require('koa')
 const fs = require('fs')
 const router = require('./router')
-
 const app = new Koa()
+
+const bodyParser = require('koa-bodyparser')
+
+app.use(bodyParser())
 
 
 app.use(router.routes()).use(router.allowedMethods())
@@ -23,40 +26,10 @@ app.use(async (ctx) => {
     `
     ctx.body = html
   } else if (ctx.method === 'POST') {
-    ctx.body = await parsePostData(ctx)
+    ctx.body = ctx.request.body
   }
 })
-// 对于POST请求的处理，koa2没有封装获取参数的方法，需要通过解析上下文context中的原生node.js请求对象req，将POST表单数据解析成query string（例如：a=1&b=2&c=3），再将query string 解析成JSON格式（例如：{"a":"1", "b":"2", "c":"3"}）
-// ctx.req == node.request  ctx.res == node.response  
-function parsePostData(ctx) {
-  return new Promise((res, rej) => {
-    try {
-      let postData = ''
-      ctx.req.on('data', data => {
-        postData += data
-      })
-      ctx.req.on('end', function () {
-        let parseData = parseQueryStr(postData)
-        res(parseData)
-      })
-    } catch (err) {
-      rej(err)
-    }
-  })
-}
 
-// post请求的字符串转json对象
-// 将POST请求参数字符串解析成JSON
-function parseQueryStr( queryStr ) {
-  let queryData = {}
-  let queryStrList = queryStr.split('&')
-  console.log( queryStrList )
-  for (  let [ index, queryStr ] of queryStrList.entries()  ) {
-    let itemList = queryStr.split('=')
-    queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
-  }
-  return queryData
-}
 
 
 app.listen(3000)
