b6ed0a3f3e58d1482426833774ace4b6a23eaefe
v_yougwang <v_yougwang@tencent.com>
2019-12-18 20:33:55 +0800

    koa-jsonp middleware

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 const Koa = require('koa')
 const app = new Koa()
+const jsonp = require('koa-jsonp')
 
+app.use(jsonp())
 
 app.use(async (ctx) => {
-  if (ctx.method === 'GET' && ctx.url.split('?')[0] === '/getData.jsonp') {
-    console.log(ctx)
-    let callbackName = ctx.query.callback || 'callback'
-    let returnData = {
-      success: true,
-      data: {
-        text: 'this is a jsonp api',
-        time: new Date().getTime(),
-      }
+  let returnData = {
+    success: true,
+    data: {
+      text: 'this is a jsonp api',
+      time: new Date().getTime()
     }
-
-    let jsonpStr = `;${callbackName}(${JSON.stringify(returnData)})`
-
-    ctx.type = 'text/javascript'
-
-    ctx.body = jsonpStr
-  } else {
-    ctx.body = 'hellp jsonp'
   }
+  ctx.body = returnData
 })
 
-
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('[demo] jsonp is starting at port 3000')
+})
