fe2d97e635d04086ff9f425f0f7858b48b333a1b
v_yougwang <v_yougwang@tencent.com>
2019-12-18 14:55:35 +0800

    koa-router中间件

--- router/home.js
+++ router/home.js
@@ -2,14 +2,14 @@ const Router = require('koa-router')
 
 const router = new Router()
 
-const home = router.get('/', async (ctx) => {
-  let html = `
-    <p><a href="/404">404.html</a></p>
-    <p><a href="/index">index.html</a></p>
-  `
-  ctx.body = html
-})
-
-module.exports = {
-  home
-}
+module.exports = router
+  .get('/', async (ctx) => {
+    let html = `
+      <p><a href="/page/404">404.html</a></p>
+      <p><a href="/index">index.html</a></p>
+    `
+    ctx.body = html
+  })
+  .get('index', async (ctx) => {
+    ctx.body = 'index page!!!'
+  })
--- router/index.js
+++ router/index.js
@@ -1,9 +1,10 @@
 const Router = require('koa-router')
 
-const { home } = require('./home')
-
 const router = new Router()
 
+const home = require('./home')
+const page = require('./page')
 router.use('/', home.routes(), home.allowedMethods())
+router.use('/page', page.routes(), page.allowedMethods())
 
 module.exports = router
