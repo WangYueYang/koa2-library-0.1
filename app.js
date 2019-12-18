2169414e8d0dc239d3cbd51bc313890c7b2106ae
v_yougwang <v_yougwang@tencent.com>
2019-12-18 19:43:08 +0800

    koa 文件上传

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const Koa = require('koa')
-const views = require('koa-views')
 const path = require('path')
-
-const router = require('./router')
-
 const app = new Koa()
 
+const { uploadFile } = require('./util/upload')
 
-
-app.use(router.routes()).use(router.allowedMethods())
-app.use(views(path.join(__dirname, './view'), {
-  extension: 'ejs'
-}))
 
 app.use(async (ctx) => {
-  let title = 'hello ejs'
-  await ctx.render('index', {
-    title,
-  })
+  if (ctx.url === '/' && ctx.method === 'GET') {
+    // 当GET请求时候返回表单页面
+    let html = `
+      <h1>koa2 upload demo</h1>
+      <form method="POST" action="/upload.json" enctype="multipart/form-data">
+        <p>file upload</p>
+        <span>picName:</span><input name="picName" type="text" /><br/>
+        <input name="file" type="file" /><br/><br/>
+        <button type="submit">submit</button>
+      </form>
+    `
+    ctx.body = html
+
+  } else if (ctx.url === '/upload.json' && ctx.method === 'POST') {
+    // 上传文件请求处理
+    let result = { success: false }
+    let serverFilePath = path.join(__dirname, 'upload-files')
+
+    // 上传文件事件
+    result = await uploadFile(ctx, {
+      fileType: 'album', // common or album
+      path: serverFilePath
+    })
+
+    ctx.body = result
+  }
 })
 
 
